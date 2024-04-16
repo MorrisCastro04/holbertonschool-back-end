b263b7cf6993fd95cea7cb61a1d4212a8e2231f6
MorrisCastro04 <jomarcastro2004@gmail.com>
2024-04-16 16:14:46 -0400

    convert to csv

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,35 @@
+import csv
+import requests
+import sys
+
+
+def gather_data_from_API(user_id):
+    """
+        Retrieves user information and tasks
+        from an API based on the given user ID.
+    """
+    response = requests.get(
+        f"https://jsonplaceholder.typicode.com/users/{user_id}")
+    if response.status_code == 200:
+        user_name = response.json()["name"]
+    response2 = requests.get(
+        f"https://jsonplaceholder.typicode.com/todos?userId={user_id}")
+    if response2.status_code == 200:
+        tasks = response2.json()
+        number_tasks = len(tasks)
+    completed_tasks = []
+    for task in tasks:
+        if task["completed"]:
+            completed_tasks.append(task)
+
+    data = []
+    with open(f"{user_id}.csv", 'w', newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for line in response2.json():
+            data.append([user_id, user_name, line["completed"], line["title"]])
+        writer.writerows(data)
+
+
+if __name__ == '__main__':
+    """call the funcion"""
+    gather_data_from_API(int(sys.argv[1]))
