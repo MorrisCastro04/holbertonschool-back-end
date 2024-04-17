6241bec25a26f554b75b7ac4f418e4215e86c767
MorrisCastro04 <jomarcastro2004@gmail.com>
2024-04-16 20:52:58 -0400

    list all employees todos

---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""""list of all todos"""
+import json
+import requests
+
+
+def list_of_todos():
+    response = requests.get(
+        f"https://jsonplaceholder.typicode.com/users")
+    users = response.json()
+    users_id = []
+    for user in users:
+        users_id.append(user['id'])
+
+    for ids in users_id:
+        response = requests.get(
+            f"https://jsonplaceholder.typicode.com/users/{ids}")
+        response2 = requests.get(
+            f"https://jsonplaceholder.typicode.com/todos?userId={ids}")
+        employee_name = response.json()["username"]
+
+        tasks = response2.json()
+
+        data_dict = {ids: []}
+        for task in tasks:
+            json_format = {
+                "username": employee_name,
+                "task": task["title"],
+                "completed": task["completed"]
+            }
+            data_dict[ids].append(json_format)
+    with open("all_employees_todos.json", "w") as json_file:
+        json.dump(data_dict, json_file)
+
+
+if __name__ == '__main__':
+    list_of_todos()
