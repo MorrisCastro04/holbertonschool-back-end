38fb0d1d7a2de6c5f9fc7453406509b6ac0f319f
MorrisCastro04 <jomarcastro2004@gmail.com>
2024-04-16 20:28:35 -0400

    change from username to name

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -13,7 +13,7 @@ def gather_data_from_API(user_id):
     response = requests.get(
         f"https://jsonplaceholder.typicode.com/users/{user_id}")
     if response.status_code == 200:
-        user_name = response.json()["username"]
+        user_name = response.json()["name"]
     response2 = requests.get(
         f"https://jsonplaceholder.typicode.com/todos?userId={user_id}")
     if response2.status_code == 200:
